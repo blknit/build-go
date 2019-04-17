079b734cdbf06c0a96b5fe5be5d9205d2bed438f
liutongqing <liutongqing@gmail.com>
2019-04-16 22:27:21 -0700

    update

--- termux/install.sh
+++ termux/install.sh
@@ -1,17 +1,17 @@
-#pkg update
-#pkg upgrade
+pkg update
+pkg upgrade
 
-#pkg install sshd -y
-#pkg install ctags -y
-#pkg install vim-python -y
-#pkg install tmux -y
-#pkg install git -y
-#pkg install golang -y
-#pkg install curl
+pkg install sshd -y
+pkg install ctags -y
+pkg install vim-python -y
+pkg install tmux -y
+pkg install git -y
+pkg install golang -y
+pkg install curl
 
-#sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrc ~/.vimrc
 
-vim +PlugInstall +qall
+vim +PlugInstall +GoInstallBinaries +qall
